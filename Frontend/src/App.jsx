46e58574ea3c00f31f1528d64908571900a1f28b
BuddhikaSadun <sadunjayamaha917@gmail.com>
2025-07-31 12:41:46 +0530

    React app route path changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import Login from "./components/Login.jsx";
-import AdminProfile from "./components/AdminProfile.jsx";
-import Salary from "./components/Salary/Salary.jsx";
-import Leave from "./components/Attendance/Leave.jsx";
-import StaffProfile from "./components/StaffProfile/StaffProfile.jsx";
+import AdminProfile from "./components/Admin/AdminProfile/AdminProfile.jsx";
+import Salary from "./components/Admin/Salary/AdminSalaryAll.jsx";
+import Leave from "./components/Admin/Leave/AdminLeave.jsx";
+
+import AddStaffLeave from "./components/Staff/StaffLeave/AddStaffLeave.jsx";
+import StaffProfile from "./components/Staff/StaffProfile/StaffProfile.jsx";
+import StaffSalary from "./components/Staff/StaffSalary/StaffSalary.jsx";
+import StaffLeave from "./components/Staff/StaffLeave/StaffLeave.jsx";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,11 +15,17 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+          {/* Admin routes */}
           <Route path="/" element={<Login />} />
-          <Route path="/admin/employee" element={<AdminProfile />} />
-          <Route path="/salary" element={<Salary />} />
-          <Route path="/leave" element={<Leave />} />
+          <Route path="/admin/profile" element={<AdminProfile />} />
+          <Route path="/admin/salary/all" element={<Salary />} />
+          <Route path="/admin/leave" element={<Leave />} />
+
+          {/* Staff routes */}
           <Route path="/staff/profile" element={<StaffProfile />} />
+          <Route path="/staff/salary" element={<StaffSalary />} />
+          <Route path="/staff/leave" element={<StaffLeave />} />
+          <Route path="/staff/leave/add" element={<AddStaffLeave />} />
         </Routes>
       </BrowserRouter>
     </div>
